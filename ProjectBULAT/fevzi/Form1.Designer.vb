152897293079276b02b7a5139f7bd4b9d663e254
NullV0 <80893651+NullV0@users.noreply.github.com>
2021-04-04 14:08:27 +0300

    Add files via upload

---
+++ ProjectBULAT/fevzi/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ımageList1 = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(893, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(10, 181)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.Window
+        Me.Label2.Location = New System.Drawing.Point(-2, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(905, 18)
+        Me.Label2.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(114, 75)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.Window
+        Me.Label3.Location = New System.Drawing.Point(-2, 181)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(905, 18)
+        Me.Label3.TabIndex = 3
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(1, 99)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(114, 82)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Credit Valley", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(573, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 80)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "YARIŞ BAŞLASIN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(95, 217)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(163, 112)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(307, 257)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(169, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(304, 226)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(219, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Resimde gördüğünüz hücre hangi evrededir?"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(619, 341)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Cevapla"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(353, 283)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Cevapla"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ımageList1
+        '
+        Me.ımageList1.ImageStream = CType(resources.GetObject("ımageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ımageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ımageList1.Images.SetKeyName(0, "1.gif")
+        Me.ımageList1.Images.SetKeyName(1, "sitokinez1.png")
+        Me.ımageList1.Images.SetKeyName(2, "anafaz.png")
+        Me.ımageList1.Images.SetKeyName(3, "anafaz1.png")
+        Me.ımageList1.Images.SetKeyName(4, "interfaz.png")
+        Me.ımageList1.Images.SetKeyName(5, "interfaz1.png")
+        Me.ımageList1.Images.SetKeyName(6, "metafaz.png")
+        Me.ımageList1.Images.SetKeyName(7, "metafaz1.png")
+        Me.ımageList1.Images.SetKeyName(8, "profaz.png")
+        Me.ımageList1.Images.SetKeyName(9, "profaz1.png")
+        Me.ımageList1.Images.SetKeyName(10, "telofaz.png")
+        Me.ımageList1.Images.SetKeyName(11, "telofaz1.png")
+        Me.ımageList1.Images.SetKeyName(12, "anafaz2.png")
+        Me.ımageList1.Images.SetKeyName(13, "metafaz2.png")
+        Me.ımageList1.Images.SetKeyName(14, "telofaz2.png")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(901, 326)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PROJECT: FEVZİ"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ımageList1 As ImageList
+End Class
---
+++ ProjectBULAT/fevzi/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+    Public at1hiz, at2hiz, bitis As Integer
+    Public cevap As String
+    Dim var As String
+    Dim intPic As Integer
+    Dim rand As New Random
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Timer1.Start()
+        Timer2.Start()
+        Dim intPic As Integer
+        intPic = Int((15 * Rnd()) + 1)
+        PictureBox3.Image = ımageList1.Images(intPic)
+        If intPic = 1 Then
+            cevap = "sitokinez"
+        End If
+        If intPic = 2 Then
+            cevap = "anafaz"
+        End If
+        If intPic = 3 Then
+            cevap = "anafaz"
+        End If
+        If intPic = 4 Then
+            cevap = "interfaz"
+        End If
+        If intPic = 5 Then
+            cevap = "interfaz"
+        End If
+        If intPic = 6 Then
+            cevap = "metafaz"
+        End If
+        If intPic = 7 Then
+            cevap = "metafaz"
+        End If
+        If intPic = 8 Then
+            cevap = "profaz"
+        End If
+        If intPic = 9 Then
+            cevap = "profaz"
+        End If
+        If intPic = 10 Then
+            cevap = "telofaz"
+        End If
+        If intPic = 11 Then
+            cevap = "telofaz"
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        var = TextBox1.Text
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim intPic As Integer
+        Dim rand As New Random
+
+        var = TextBox1.Text
+
+        If var = cevap Then
+            PictureBox2.Left = PictureBox2.Left + at2hiz + 50
+            intPic = Int((14 * Rnd()) + 1)
+            PictureBox3.Image = ımageList1.Images(intPic)
+
+        End If
+        If intPic = 1 Then
+            cevap = "sitokinez"
+        ElseIf intPic = 2 Then
+            cevap = "anafaz"
+
+        ElseIf intPic = 3 Then
+            cevap = "anafaz"
+
+        ElseIf intPic = 4 Then
+            cevap = "interfaz"
+
+        ElseIf intPic = 5 Then
+            cevap = "interfaz"
+
+        ElseIf intPic = 6 Then
+            cevap = "metafaz"
+
+        ElseIf intPic = 7 Then
+            cevap = "metafaz"
+
+        ElseIf intPic = 8 Then
+            cevap = "profaz"
+
+        ElseIf intPic = 9 Then
+            cevap = "profaz"
+
+        ElseIf intPic = 10 Then
+            cevap = "telofaz"
+
+        ElseIf intPic = 11 Then
+            cevap = "telofaz"
+
+        ElseIf intPic = 12 Then
+            cevap = "anafaz"
+
+        ElseIf intPic = 13 Then
+            cevap = "metafaz"
+
+        ElseIf intPic = 14 Then
+            cevap = "telofaz"
+        End If
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'FevziBOT
+        Randomize()
+        at1hiz = Math.Ceiling(Rnd() * 10)
+        PictureBox1.Left = PictureBox1.Left + at1hiz
+        'Player
+        Randomize()
+        at2hiz = 4
+        PictureBox2.Left = PictureBox2.Left + at2hiz
+        If PictureBox1.Left > Label1.Left Then
+            Timer1.Stop()
+            MsgBox("FevziBOT kazandı!")
+            PictureBox2.Left = 0
+            PictureBox1.Left = 0
+        End If
+
+        If PictureBox2.Left > Label1.Left Then
+            Timer1.Stop()
+            MsgBox("Sen Kazandın!")
+            PictureBox2.Left = 0
+            PictureBox1.Left = 0
+        End If
+
+
+    End Sub
+End Class
